ae4c4bb101e306556d9f19715b40e67358954cd5
wuhaoyi <wuhaoyi@bytedance.com>
2026-04-04 23:07:48 +0800

    delete command

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.commands.onCommand.addListener(async (command) => {
-  if (command !== "open-bookmark-picker") return
-
-  try {
-    await chrome.action.openPopup()
-  } catch {
-    await chrome.action.openPopup({})
-  }
-})
